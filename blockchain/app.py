df642d652b069347716ad11b1acba9cb35af213c
Lucas May Petry <lucasmaypetry@gmail.com>
2017-11-13 16:34:52 -0200

    Block

--- blockchain/app.py
+++ blockchain/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_restful import Resource, Api
 from flask_jsonpify import jsonify
-from base64 import b64encode, b64decode
 from Crypto.PublicKey import RSA
 from smart_presence import SmartPresence
 
@@ -11,7 +10,7 @@ api = Api(app)
 class Transaction(Resource):
 
     def post(self):
-        block = {
+        transaction = {
             'id' : request.json['id'],
             'entity_pbk' : request.json['entity_pbk'],
             'authority_pbk' : request.json['authority_pbk'],
@@ -22,10 +21,21 @@ class Transaction(Resource):
             'authority_signature' : request.json['authority_signature']
         }
 
-        sp = SmartPresence.fromJSON(block)
+        sp = SmartPresence.fromJSON(transaction)
 
         # TO-DO process transaction sp
 
         return {'status' : 'success'}
 
+class Block(Resource):
+
+    def post(self):
+        block = {
+        }
+
+        # TO-DO process block
+
+        return {'status' : 'success'}
+
 api.add_resource(Transaction, '/transaction')
+api.add_resource(Block, '/block')
